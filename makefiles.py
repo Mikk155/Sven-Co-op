03a08252a290fb54377cf8048d7b440c1fa2d0f4
Mikk <martimsanchez155@gmail.com>
2024-04-04 15:13:27 -0300

    Resource manager script

--- makefiles.py
+++ makefiles.py
@@ -1,39 +1,51 @@
-#!/usr/bin/env python3
 import os
 import sys
+import shutil
 
-def main():
+def RepackResources( res_file ):
+
+    dependencies_dir = os.path.join(os.path.dirname(__file__), 'dependencies')
+
+    if not os.path.exists(dependencies_dir):
+
+        os.makedirs(dependencies_dir)
+
+    with open(res_file, 'r') as f:
+
+        for line in f:
+
+            file_path = line.strip().strip('"')
+
+            destination_path = os.path.join(dependencies_dir, file_path)
+
+            os.makedirs(os.path.dirname(destination_path), exist_ok=True)
+
+            if destination_path.endswith( '.res' ):
+
+                RepackResources( f"resources/{file_path}" )
+
+                continue
+
+            try:
+
+                shutil.copyfile(file_path, destination_path)
+
+                print(f"{file_path}")
+
+            except Exception as e:
+
+                print(f"Warning! couldn't copy {file_path}: {e}")
+
+if __name__ == "__main__":
+
     if len( sys.argv ) != 2:
-        print("Usage: {} <file.res>".format(sys.argv[0]))
+
         sys.exit(1)
 
     res_file = sys.argv[1]
 
-    if not os.path.isfile(res_file):
-        print("¡Warning {} not exists".format(res_file))
-        sys.exit(1)
+    if not os.path.exists(res_file):
 
-    temp_dir = "dependencies"
-    os.makedirs(temp_dir, exist_ok=True)
-
-    dependencies = []
-    with open(res_file, 'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            dependency = line.replace( '"', '' )
-            dependency = dependency.replace( '\n', '' )
-            dependencies.append(dependency)
-
-    Path = os.path.join(os.path.dirname(__file__) )
-
-    for dependency in dependencies:
-        print( f'{dependency}\n')
-        if os.path.exists( f'{Path}/{dependency}' ):
-            os.system("cp {} {}".format(dependency, temp_dir))
-        else:
-            print( f"Warning: {Path}/{dependency} no exists!" )
-
-    print("All done.")
-
-if __name__ == "__main__":
-    main()
+        print(f"Warning: File {res_file} not found!")
+    else:
+        RepackResources( res_file )
