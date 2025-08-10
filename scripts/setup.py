8f9056b0f51fb244276a72df33ca4f48de27986d
Mikk155 <martimsanchez155@gmail.com>
2025-08-09 22:32:38 -0300

    Update setup script

--- scripts/setup.py
+++ scripts/setup.py
@@ -43,3 +43,47 @@ del MyWorkspace;
 from utils.Logger import Logger, LoggerSetLevel, LoggerLevel;
 LoggerSetLevel( LoggerLevel.AllLoggers );
 g_Logger: Logger = Logger( "Setup" );
+
+PathSvenCoop: str = Path.enter( "steamapps", "common", "Sven Co-op", CurrentDir= Path.GetSteamInstallation() );
+PathPluginsAddon: str = Path.enter( "svencoop_addon", "default_plugins.txt", CurrentDir=PathSvenCoop, CreateIfNoExists=True, SupressWarning=True );
+
+MPManagerHeader: str = '''\n
+\t"plugin"
+\t{
+\t\t"name" "MPManager"
+\t\t"script" "MPManager"
+\t\t"concommandns" "mpm"
+\t}\n
+''';
+
+if not os.path.exists( PathPluginsAddon ):
+#
+    with open( PathPluginsAddon, "w" ) as default_plugins:
+    #
+        g_Logger.info( "Generating <c>{}<>", PathPluginsAddon );
+        default_plugins.write( '''"plugins"\n{{{}}}'''.format( MPManagerHeader ) );
+    #
+#
+else:
+#
+    with open( PathPluginsAddon, "r" ) as default_plugins:
+    #
+        Content: str = default_plugins.read();
+
+        if Content.find( "\"script\" \"MPManager\"" ) == -1:
+        #
+            g_Logger.info( "Updating <c>{}<>", PathPluginsAddon );
+
+            IndexEnd: int = Content.rfind( "}" );
+
+            Content = Content[ : IndexEnd - 1 ] + MPManagerHeader + Content[ IndexEnd ];
+
+            with open( PathPluginsAddon, "w" ) as default_plugins_write:
+            #
+                default_plugins_write.write( Content );
+            #
+        #
+    #
+#
+
+del PathPluginsAddon;
