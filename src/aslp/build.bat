69cd5c4b5311c322331030c67b99f9cb8609de93
Mikk155 <martimsanchez155@gmail.com>
2025-08-19 20:31:11 -0300

    build script

---
+++ src/aslp/build.bat
@@ -0,0 +1,22 @@
+@echo off
+
+setlocal
+
+if not exist bin (
+    mkdir bin
+    cd bin
+    cmake ..
+    cd ..
+)
+
+cmake --build bin --config Debug --clean-first
+
+if %ERRORLEVEL% NEQ 0 (
+    echo ERROR
+    pause
+    exit /b %ERRORLEVEL%
+)
+
+@REM close sven exe + force move file?
+
+pause
