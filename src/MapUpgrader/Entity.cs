be4d22656f18d6ec91602bcab9e98bd57b4f50ab
Mikk155 <martimsanchez155@gmail.com>
2025-08-23 14:40:10 -0300

    Compact C# Entity's descriptions

--- src/MapUpgrader/Entity.cs
+++ src/MapUpgrader/Entity.cs
@@ -1,19 +1,13 @@
 #pragma warning disable IDE1006 // Naming Styles
-/// <summary>
-/// Represents a entity in the BSP
-/// </summary>
+/// <summary>Represents a entity in the BSP</summary>
 public class Entity( int index )
 {
-    /// <summary>
-    /// Represents all the key-values in this entity
-    /// </summary>
+    /// <summary>Represents all the key-values in this entity</summary>
     public Dictionary<string, string> keyvalues = new Dictionary<string, string>();
 
     public readonly int index = index;
 
-    /// <summary>
-    /// Represents the current index of this entity in the BSP entity data
-    /// </summary>
+    /// <summary>Represents the current index of this entity in the BSP entity data</summary>
     public int EntIndex
     {
         get
@@ -22,9 +16,7 @@ public class Entity( int index )
         }
     }
 
-    /// <summary>
-    /// Classname of the entity. if this is not a valid entity in the FGD the program will only warn.
-    /// </summary>
+    /// <summary>Classname of the entity. if this is not a valid entity in the FGD the program will only warn.</summary>
     public string classname
     {
         get
@@ -37,109 +29,67 @@ public class Entity( int index )
         }
     }
 
-    /// <summary>
-    /// Get a key's value in string form
-    /// </summary>
-    /// <param name="key">Entity Key</param>
-    /// <returns>the entity key's value</returns>
+    /// <summary>Get a key's value in string form</summary>
     public string GetString( string key )
     {
         return keyvalues.TryGetValue( key, out string? value ) ? value.ToString() : "";
     }
 
-    /// <summary>
-    /// Set a key's value in string form
-    /// </summary>
-    /// <param name="key">Entity Key</param>
-    /// <param name="value">New entity key's Value</param>
+    /// <summary>Set a key's value in string form</summary>
     public void SetString( string key, string value )
     {
         keyvalues[ key ] = value;
     }
 
-    /// <summary>
-    /// Get a key's value in integer form
-    /// </summary>
-    /// <param name="key">Entity Key</param>
-    /// <returns>the entity key's value</returns>
+    /// <summary>Get a key's value in integer form</summary>
     public int GetInteger( string key )
     {
         return keyvalues.TryGetValue( key, out string? value ) ? int.TryParse( value.ToString(), out int ivalue ) ? ivalue : 0 : 0;
     }
 
-    /// <summary>
-    /// Set a key's value in integer form
-    /// </summary>
-    /// <param name="key">Entity Key</param>
-    /// <param name="value">New entity key's Value</param>
+    /// <summary>Set a key's value in integer form</summary>
     public void SetInteger( string key, int value )
     {
         keyvalues[ key ] = value.ToString();
     }
 
-    /// <summary>
-    /// Get a key's value in float form
-    /// </summary>
-    /// <param name="key">Entity Key</param>
-    /// <returns>the entity key's value</returns>
+    /// <summary>Get a key's value in float form</summary>
     public float GetFloat( string key )
     {
         return keyvalues.TryGetValue( key, out string? value ) ? float.TryParse( value.ToString(), out float ivalue ) ? ivalue : 0.0f : 0.0f;
     }
 
-    /// <summary>
-    /// Set a key's value in float form
-    /// </summary>
-    /// <param name="key">Entity Key</param>
-    /// <param name="value">New entity key's Value</param>
+    /// <summary>Set a key's value in float form</summary>
     public void SetFloat( string key, float value )
     {
         keyvalues[ key ] = value.ToString();
     }
 
-    /// <summary>
-    /// Get a key's value in bool form (0/1)
-    /// </summary>
-    /// <param name="key">Entity Key</param>
-    /// <returns>the entity key's value</returns>
+    /// <summary>Get a key's value in bool form (0/1)</summary>
     public bool GetBool( string key )
     {
         return keyvalues.TryGetValue( key, out string? value ) ? int.TryParse( value.ToString(), out int ivalue ) ? ivalue != 0 : false : false;
     }
 
-    /// <summary>
-    /// Set a key's value in bool form (0/1)
-    /// </summary>
-    /// <param name="key">Entity Key</param>
-    /// <param name="value">New entity key's Value</param>
+    /// <summary>Set a key's value in bool form (0/1)</summary>
     public void SetBool( string key, bool value )
     {
         keyvalues[ key ] = value.ToString();
     }
 
-    /// <summary>
-    /// Get a key's value in bool form (0/1)
-    /// </summary>
-    /// <param name="key">Entity Key</param>
-    /// <returns>the entity key's value</returns>
+    /// <summary>Get a key's value in bool form (0/1)</summary>
     public Vector GetVector( string key )
     {
         return keyvalues.TryGetValue( key, out string? value ) ? new Vector( value ) : Vector.vecZero;
     }
 
-    /// <summary>
-    /// Set a key's value in bool form (0/1)
-    /// </summary>
-    /// <param name="key">Entity Key</param>
-    /// <param name="value">New entity key's Value</param>
+    /// <summary>Set a key's value in bool form (0/1)</summary>
     public void SetVector( string key, Vector value )
     {
         keyvalues[ key ] = value.ToString();
     }
 
-    /// <summary>
-    /// Return the entity in the .ent format
-    /// </summary>
+    /// <summary>Return the entity in the .ent format</summary>
     public override string ToString()
     {
         System.Text.StringBuilder s = new System.Text.StringBuilder();
