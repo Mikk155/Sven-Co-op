9e3be4e34e7896f25c65b1f6f63ee89ca097d643
Mikk155 <martimsanchez155@gmail.com>
2025-08-24 15:51:24 -0300

    Start porting setup.py to .NET

---
+++ scripts/setup/Program.cs
@@ -0,0 +1,28 @@
+﻿class Setup
+{
+    private static readonly List<IProject> Projects = new List<IProject>(){
+    };
+
+    public static void Main()
+    {
+        Console.Title = "Setup";
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.Cyan;
+
+        IProject.Config( "svencoop_exe", value =>
+        {
+            if( !Directory.Exists( Path.Combine( value, "svencoop" ) ) )
+            {
+                throw new FileNotFoundException( $"Couldn't find the game's content at \"{value}\" Note the last directory must be outside of the \"svencoop\" folder" );
+            }
+            return true;
+        }, $"Input the absolute path to sven coop installation" );
+
+        foreach( IProject project in Projects )
+        {
+            project.Initialize();
+        }
+
+        Console.ResetColor();
+    }
+}
