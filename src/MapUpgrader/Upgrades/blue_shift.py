c504ad3a45747754115f8d686636236092439485
Mikk155 <martimsanchez155@gmail.com>
2025-09-13 20:47:21 -0300

    Start with blue shift

--- src/MapUpgrader/Upgrades/blue_shift.py
+++ src/MapUpgrader/Upgrades/blue_shift.py
@@ -7,3 +7,21 @@ def register( context: UpgradeContext ) -> None:
     context.description = "Half-Life: Blue-Shift expansion";
     context.urls = [ "https://store.steampowered.com/app/130/HalfLife_Blue_Shift/" ];
 
+global assets_directory;
+assets_directory: str = "mikk/bshift";
+
+def assets( assets: Assets ) -> None:
+
+    global assets_directory;
+
+    # Textures
+    assets.Copy( "barney.wad" );
+
+    # Skyboxes
+    assets.Copy( "gfx/env/*.tga", f"gfx/env/{assets_directory}/" );
+
+    # Maps
+    assets.Copy( "maps/*.bsp" );
+
+    # Music
+    assets.Copy( "media/*.mp3", f"sound/{assets_directory}/music/" );
