a3614dcc0c25c1f1b17d72bb94b56e2e287b9b5c
Mikk155 <martimsanchez155@gmail.com>
2025-09-15 21:55:53 -0300

    Implement a deprecation wrapper

--- src/GoldSrc2Sven/scripts/netapi/NET.py
+++ src/GoldSrc2Sven/scripts/netapi/NET.py
@@ -8,3 +8,36 @@ from netapi.ConsoleColor import ConsoleColor;
 from netapi.CFG import CFG;
 from netapi.MapUpgrades import MapUpgrades;
 from netapi.IMapUpgrade import IMapUpgrade;
+
+import functools;
+from typing import Callable, Optional;
+
+def deprecated( NewMethod: str, Critical: Optional[bool] = False, Exit: Optional[bool] = False ) -> Callable:
+
+    '''
+        Mark a method as deprecated.
+
+        ``NewMethod``: Name of a new method that should be used.
+
+        ``Critical`` if True, the method won't execute its code.
+
+        ``Exit`` If True the program exit with error 1.
+    '''
+
+    def decorator( func ) -> Callable:
+
+        @functools.wraps( func )
+
+        def wrapper( *args, **kwargs ):
+
+            if Critical:
+    
+                print( f"ERROR! {func.__name__} is DEPRECATED Use {NewMethod} instead!" );
+
+                return None;
+
+            print( f"WARNING! {func.__name__} is DEPRECATED Use {NewMethod} instead!" );
+
+        return wrapper;
+
+    return decorator;
