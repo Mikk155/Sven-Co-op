1f69d75000df6a16bbfea6566d48e8d757d24e53
Mikk155 <martimsanchez155@gmail.com>
2025-09-28 14:12:26 -0300

    Correct folder name and add url

--- src/GoldSrc2Sven/scripts/counterstrike16.py
+++ src/GoldSrc2Sven/scripts/counterstrike16.py
@@ -5,10 +5,10 @@ def register_context( context: Upgrade ) -> None:
     context.mod = "cstrike";
     context.title = "Counter-Strike";
     context.description = "Counter-Strike";
-    context.urls = [ "" ];
+    context.urls = [ "https://store.steampowered.com/app/10/CounterStrike/" ];
 
 global assets_directory;
-assets_directory: str = "mikk/bshift";
+assets_directory: str = "mikk/cstrike";
 
 def install_assets( context: Assets ) -> None:
 
