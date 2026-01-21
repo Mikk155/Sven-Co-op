ee5edf334e1125eaf4f68542673c6ec11eb4e977
Mikk155 <martimsanchez155@gmail.com>
2026-01-21 20:42:52 -0300

    Document engine-defined network messages

--- src/aslp/header/generate_as_networking.h
+++ src/aslp/header/generate_as_networking.h
@@ -8,42 +8,110 @@
 
 class CNetworkMessageAPI
 {
-    public:
+	public:
 
-        // Registered network message information
-        struct MessageData
-        {
-            // String name in the client side.
-            std::string Name;
+		// Registered network message information
+		struct MessageData
+		{
+			// String name in the client side.
+			std::string Name;
 
-            // Number of bytes sent.
-            int Bytes;
+			// ID in the server side.
+			int Id;
 
-            // ID in the server side.
-            int Id;
+			// Number of bytes sent.
+			int Bytes;
 
-            // Byte types and ordering to sent.
-            std::vector<CASNetworkMessageByteType> Data = {};
+			// Byte types and ordering to sent.
+			std::vector<CASNetworkMessageByteType> Data = {};
 
-            // Information string (Cleared after documentation is writted)
-            std::string Info;
-        };
+			// Information string (Cleared after documentation is writted)
+			std::string Info;
+		};
 
-    private:
+	private:
 
-        // Every Network Message registered.
-        std::vector<MessageData> m_RegisteredNetworkMessages = {
-            { .Name = "SVC_BAD", .Id = 0 }
-        };
+		// Every Network Message registered.
+		std::vector<MessageData> m_RegisteredNetworkMessages = {
+			{ .Name = "Bad", .Id = 0 },
+			{ .Name = "Nop", .Id = 1 },
+			{ .Name = "Disconnect", .Id = 2 },
+			{ .Name = "Event", .Id = 3 },
+			{ .Name = "Version", .Id = 4 },
+			{ .Name = "SetView", .Id = 5 },
+			{ .Name = "Sound", .Id = 6 },
+			{ .Name = "Time", .Id = 7 },
+			{ .Name = "Print", .Id = 8 },
+			{ .Name = "StuffText", .Id = 9 },
+			{ .Name = "SetAngle", .Id = 10 },
+			{ .Name = "ServerInfo", .Id = 11 },
+			{ .Name = "LightStyle", .Id = 12 },
+			{ .Name = "UpdateUserInfo", .Id = 13 },
+			{ .Name = "DeltaDescription", .Id = 14 },
+			{ .Name = "ClientData", .Id = 15 },
+			{ .Name = "StopSound", .Id = 16 },
+			{ .Name = "Pings", .Id = 17 },
+			{ .Name = "Particle", .Id = 18 },
+			{ .Name = "Damage", .Id = 19 },
+			{ .Name = "SpawnStatic", .Id = 20 },
+			{ .Name = "EventReliable", .Id = 21 },
+			{ .Name = "SpawnBaseline", .Id = 22 },
+			{ .Name = "TempEntity", .Id = 23 },
+			{ .Name = "SetPause", .Id = 24 },
+			{ .Name = "SignOnNum", .Id = 25 },
+			{ .Name = "CenterPrint", .Id = 26 },
+			{ .Name = "KilledMonster", .Id = 27 },
+			{ .Name = "FoundSecret", .Id = 28 },
+			{ .Name = "SpawnStaticSound", .Id = 29 },
+			{ .Name = "Intermission", .Id = 30 },
+			{ .Name = "Finale", .Id = 31 },
+			{ .Name = "CdTrack", .Id = 32 },
+			{ .Name = "Restore", .Id = 33 },
+			{ .Name = "CutScene", .Id = 34 },
+			{ .Name = "WeaponAnim", .Id = 35 },
+			{ .Name = "DecalName", .Id = 36 },
+			{ .Name = "RoomType", .Id = 37 },
+			{ .Name = "AddAngle", .Id = 38, .Bytes = 2, .Info = R"(
+		/**
+		*	Add an angle on the yaw axis of the current client's view angle.
+		*	Note: When pev->fixangle is set to 2, this message is called with pev->avelocity[1] as a value.
+		*	Note: The value needs to be scaled by (65536 / 360).
+        *   Expected {} Bytes.
+		*	Structure:
+		*	short	AngleToAdd
+		**/
+)" },
+			{ .Name = "NewUserMsg", .Id = 39 },
+			{ .Name = "PacketEntities", .Id = 40 },
+			{ .Name = "DeltaPacketEntities", .Id = 41 },
+			{ .Name = "Choke", .Id = 42 },
+			{ .Name = "ResourceList", .Id = 43 },
+			{ .Name = "NewMoveVars", .Id = 44 },
+			{ .Name = "ResourceRequest", .Id = 45 },
+			{ .Name = "Customization", .Id = 46 },
+			{ .Name = "CrosshairAngle", .Id = 47 },
+			{ .Name = "SoundFade", .Id = 48 },
+			{ .Name = "FileTxferFailed", .Id = 49 },
+			{ .Name = "Hltv", .Id = 50 },
+			{ .Name = "Director", .Id = 51 },
+			{ .Name = "VoiceInit", .Id = 52 },
+			{ .Name = "VoiceData", .Id = 53 },
+			{ .Name = "SendExtraInfo", .Id = 54 },
+			{ .Name = "TimeScale", .Id = 55 },
+			{ .Name = "ResourceLocation", .Id = 56 },
+			{ .Name = "SendCvarValue", .Id = 57 },
+			{ .Name = "SendCvarValue2", .Id = 58 }
+		};
+		// Information for these has been taken from https://wiki.alliedmods.net/Half-Life_1_Engine_Messages
 
-    public:
+	public:
 
-        MessageData* GetMessageData( const std::string& name );
-        MessageData* GetMessageData( int id );
+		MessageData* GetMessageData( const std::string& name );
+		MessageData* GetMessageData( int id );
 
-        void Initialize( const asIScriptEngine* engine );
+		void Initialize( const asIScriptEngine* engine );
 
-        void Register( const char* name, int bytes, int id );
+		void Register( const char* name, int bytes, int id );
 };
 
 inline CNetworkMessageAPI g_NetworkMessageAPI;
--- src/aslp/source/generate_as_networking.cpp
+++ src/aslp/source/generate_as_networking.cpp
@@ -5,77 +5,100 @@
 
 void CNetworkMessageAPI :: Initialize( const asIScriptEngine* engine )
 {
-    CFile file( "scripts/mikk/NetworkMessages.as", CFile::Mode::Write );
+	CFile file( "scripts/mikk/NetworkMessages.as", CFile::Mode::Write );
 
-    if( !file.IsOpen() )
-    {
-        ALERT( at_console, "[Error] Couldn't create file \"scripts/NetworkMessages.as\"\n" );
-        return;
-    }
+	if( !file.IsOpen() )
+	{
+		ALERT( at_console, "[Error] Couldn't create file \"scripts/NetworkMessages.as\"\n" );
+		return;
+	}
 
-    std::string fileContent;
-    fileContent.reserve(1024);
+	std::string fileContent;
+	fileContent.reserve(2048);
 
-    fmt::format_to( std::back_inserter( fileContent ), "namespace NetworkMessages\n{{\n\t// Network messages IDs\n\tenum Message\n\t{{\n" );
+	fmt::format_to( std::back_inserter( fileContent ), "namespace NetworkMessages\n{{\n\t// Network messages IDs\n\tenum Message\n\t{{\n" );
 
-    for( MessageData& networkMessage : m_RegisteredNetworkMessages )
-    {
-        fmt::format_to( std::back_inserter( fileContent ),
-            "\t\t{} = {}, // {} Bytes.\n", networkMessage.Name, networkMessage.Id, networkMessage.Bytes );
+	for( MessageData& networkMessage : m_RegisteredNetworkMessages )
+	{
+		if( !networkMessage.Info.empty() )
+		{
+			fmt::format_to( std::back_inserter( fileContent ), fmt::runtime( networkMessage.Info ), networkMessage.Bytes );
+ 
+			// Already documented. these string aren't needed anymore.
+			networkMessage.Info.clear();
+		}
+        else
+        {
+            switch( networkMessage.Bytes )
+            {
+                case -1:
+    			    fmt::format_to( std::back_inserter( fileContent ), "\n\t\t/**\n\t\t*\tDynamic number of bytes.\n\t\t**/\n" );
+                break;
+                case 0:
+    			    fmt::format_to( std::back_inserter( fileContent ), "\n\t\t/**\n\t\t*\tNo bytes expected.\n\t\t**/\n" );
+                break;
+                case 1:
+    			    fmt::format_to( std::back_inserter( fileContent ), "\n\t\t/**\n\t\t*\tExpected 1 Byte.\n\t\t**/\n" );
+                break;
+                default:
+    			    fmt::format_to( std::back_inserter( fileContent ), "\n\t\t/**\n\t\t*\tExpected {} Bytes.\n\t\t**/\n", networkMessage.Bytes );
+                break;
+            }
+        }
 
-        // Already documented. these string aren't needed anymore.
-        networkMessage.Info.clear();
-    }
+		fmt::format_to( std::back_inserter( fileContent ),
+			"\t\t{} = {}\n", networkMessage.Name, networkMessage.Id  );
+	}
 
-    fmt::format_to( std::back_inserter( fileContent ), "\t}}\n}}\n" );
+	fmt::format_to( std::back_inserter( fileContent ), "\t}}\n}}\n" );
 
-    if( file.Write( fileContent ) )
-    {
-        ALERT(at_console, "File \"scripts/NetworkMessages.as\" Generated suscessfully. writted %i network message's IDs\n", m_RegisteredNetworkMessages.size() );
-    }
+	if( file.Write( fileContent ) )
+	{
+		ALERT(at_console, "File \"scripts/NetworkMessages.as\" Generated suscessfully. writted %i network message's IDs\n", m_RegisteredNetworkMessages.size() );
+	}
 }
 
 CNetworkMessageAPI::MessageData* CNetworkMessageAPI :: GetMessageData( const std::string& name )
 {
-    auto listStart = m_RegisteredNetworkMessages.begin();
-    auto listEnd = m_RegisteredNetworkMessages.end();
-    auto it = std::find_if( listStart, listEnd, [&]( const auto& msg ){ return msg.Name == name; } );
-    if( it != listEnd ) {
-        return &(*it);
-    }
-    return nullptr;
+	auto listStart = m_RegisteredNetworkMessages.begin();
+	auto listEnd = m_RegisteredNetworkMessages.end();
+	auto it = std::find_if( listStart, listEnd, [&]( const auto& msg ){ return msg.Name == name; } );
+	if( it != listEnd ) {
+		return &(*it);
+	}
+	return nullptr;
 }
 
 CNetworkMessageAPI::MessageData* CNetworkMessageAPI :: GetMessageData( int id )
 {
-    auto listStart = m_RegisteredNetworkMessages.begin();
-    auto listEnd = m_RegisteredNetworkMessages.end();
-    auto it = std::find_if( listStart, listEnd, [&]( const auto& msg ){ return msg.Id == id; } );
-    if( it != listEnd ) {
-        return &(*it);
-    }
-    return nullptr;
+	auto listStart = m_RegisteredNetworkMessages.begin();
+	auto listEnd = m_RegisteredNetworkMessages.end();
+	auto it = std::find_if( listStart, listEnd, [&]( const auto& msg ){ return msg.Id == id; } );
+	if( it != listEnd ) {
+		return &(*it);
+	}
+	return nullptr;
 }
 
 void CNetworkMessageAPI :: Register( const char* name, int bytes, int id )
 {
-    std::string msgName = std::string( name );
+	std::string msgName = std::string( name );
 
-    MessageData* msg = GetMessageData( id );
+	MessageData* msg = GetMessageData( id );
 
-    if( msg != nullptr )
-    {
-        msg->Id = id;
-        msg->Bytes = bytes;
-    }
-    else
-    {
-        MessageData data{
-            .Name = std::string( name ),
-            .Bytes = bytes,
-            .Id = id
-        };
+	if( msg != nullptr )
+	{
+		msg->Id = id;
+		msg->Bytes = bytes;
+	}
+	else
+	{
+		MessageData data{
+			.Name = std::string( name ),
+			.Id = id,
+			.Bytes = bytes
+		};
 
-        m_RegisteredNetworkMessages.push_back( std::move( data ) );
-    }
+		m_RegisteredNetworkMessages.push_back( std::move( data ) );
+	}
 }
