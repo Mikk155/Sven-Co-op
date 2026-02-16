a342c29d73f1fcbc5d7aa6af76be6525dfd1ab1f
Mikk155 <martimsanchez155@gmail.com>
2026-02-16 10:11:44 -0300

    Update CMake setup, structure and documentation

--- src/aslp/build.bat
+++ src/aslp/build.bat
@@ -4,25 +4,13 @@ setlocal
 
 taskkill /F /IM "svencoop.exe"
 
-if not exist bin (
-    mkdir bin
-    cd bin
-    cmake -A Win32 ..
-    cd ..
-)
-
-cmake --build bin --config Release --clean-first
+cmake --build ../../build/aslp --config Release --clean-first
 
 if %ERRORLEVEL% NEQ 0 (
     echo ERROR
     pause
     exit /b %ERRORLEVEL%
 )
-
-set "SourceFile=C:\Users\Usuario\Documents\GitHub\Sven-Co-op\src\aslp\bin\Release\aslp.dll"
-set "DestinationDir=C:\Program Files (x86)\Steam\steamapps\common\Sven Co-op\svencoop\addons\metamod\dlls\aslp.dll"
-copy "%SourceFile%" "%DestinationDir%" /Y
-echo "Installed to %DestinationDir%"
 
 "C:\Users\Usuario\Desktop\Sven Co-op.url"
 
