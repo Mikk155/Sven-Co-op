b8ee73b60f1fb536e4497b6608025e911ab6c834
Mikk155 <martimsanchez155@gmail.com>
2026-02-16 16:51:03 -0300

    Mandatory header for easy inclusion

---
+++ src/aslp/metamod/mandatory.h
@@ -0,0 +1,14 @@
+// HLSDK
+#include <extdll.h>
+#include "enginedef.h"
+
+// Metamod
+#include <meta_api.h>
+#include "extern_hook.h"
+
+// Angelscript
+#include "aslp.h"
+#include "asext_api.h"
+#include "angelscriptlib.h"
+
+#pragma once
