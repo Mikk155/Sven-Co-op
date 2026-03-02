46d961722f5d4a6cc2dcf5e0ee948c02041b8273
Mikk155 <martimsanchez155@gmail.com>
2026-03-02 17:57:40 -0300

    string& convertToUtf8(string&)

--- src/aslp/misc/GenerateASPredefined.hpp
+++ src/aslp/misc/GenerateASPredefined.hpp
@@ -14,6 +14,27 @@
 
 #pragma once
 
+// Google AI Slop for now.
+#include <windows.h>
+// Convert from a specific source encoding (e.g., system's active code page CP_ACP) to UTF-8
+std::string& convertToUtf8(std::string& sourceStr) {
+    // 1. Convert source encoding (e.g., CP_ACP) to UTF-16 (std::wstring)
+    int sizeNeededForWstr = MultiByteToWideChar(CP_ACP, 0, sourceStr.c_str(), -1, nullptr, 0);
+    std::wstring wstr(sizeNeededForWstr, 0);
+    MultiByteToWideChar(CP_ACP, 0, sourceStr.c_str(), -1, &wstr[0], sizeNeededForWstr);
+
+    // 2. Convert UTF-16 to UTF-8 (std::string)
+    int sizeNeededForUtf8 = WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, nullptr, 0, nullptr, nullptr);
+    std::string utf8Str(sizeNeededForUtf8, 0);
+    WideCharToMultiByte(CP_UTF8, 0, wstr.c_str(), -1, &utf8Str[0], sizeNeededForUtf8, nullptr, nullptr);
+
+    // Remove the null terminators that MultiByteToWideChar/WideCharToMultiByte add
+    // when using -1 for the input length
+    utf8Str.resize(sizeNeededForUtf8 - 1);
+    sourceStr = std::move( utf8Str );
+    return sourceStr;
+}
+
 #ifdef GENERATE_AS_EXTERNAL_TEST
 #define LOG_ARGS(fmt_str, ...) fmt::print( "[GenerateASPredefined] " fmt_str "\n", __VA_ARGS__ )
 #define LOG(fmt_str) fmt::print( "[GenerateASPredefined] " fmt_str "\n" )
@@ -60,6 +81,15 @@ void Generate()
     if( auto filename = std::filesystem::current_path() / "svencoop" / "asdocs.txt"; !ParseFile( filename.string().c_str(), asdocs ) )
         return;
 
+    /*
+    // -TODO Delay these untill all generated and maybe make all these formating in a thread?
+    as_dumphooks
+    as_fs_dumpfilesystem
+    as_scriptbaseclasses
+    condebug;wait;[as_scriptbaseclasses];wait;as_scriptbaseclasses;wait;condebug 
+    as_scriptbaseclasses is a client command? Didn't output anything on dedicated server. may automate it from ClientPutInServer -TODO
+    */
+
     // Get the last console log file for AS Base classes
     string asbaseclasses;
     {
@@ -76,9 +106,10 @@ void Generate()
 
     if( !asbaseclasses.empty() )
     {
+        convertToUtf8( asbaseclasses );
+
         LOG_ARGS( "asbaseclasses: {}", asbaseclasses );
     }
-
 } // End Generate
 } // End GenerateASPredefined
 #undef LOG
