8e71fc9844e65b8458efcbc872685649ddbe8c47
Mikk155 <martimsanchez155@gmail.com>
2026-03-04 10:19:12 -0300

    Refactore json.hpp

--- src/aslp/angelscript/json.hpp
+++ src/aslp/angelscript/json.hpp
@@ -1,27 +1,28 @@
-#include <string_view>
-
-#pragma once
-
 #include <nlohmann/json.hpp>
 #include <fmt/format.h>
 
-#include <extdll.h>
-#include <meta_api.h>
-#include "aslp.h"
+#pragma once
+
+#include "mandatory.h"
 
 namespace json
 {
-    bool JsonToDict( const nlohmann::json& js, CScriptDictionary* dict )
+    /**
+     * @brief Convert the given js object into the given dict object.
+     * NOTE: If the js object is or have any array type it will also be converted into an object where the keys are the index of the values.
+     */
+    void ToDictionary( nlohmann::json& js, CScriptDictionary* dict )
     {
         if( !js.is_object() )
-            return false;
+            return;
 
         auto ASEngine = ASEXT_GetServerManager()->GetScriptEngine();
+
         auto dictionaryType = ASEngine->GetTypeInfoByName( "dictionary" );
-        int dictionaryId = ASEngine->GetTypeIdByDecl("dictionary@");
+        int dictionaryId = ASEngine->GetTypeIdByDecl( "dictionary@" );
 
         auto stringType = ASEngine->GetTypeInfoByName( "string" );
-        int stringId = ASEngine->GetTypeIdByDecl("string");
+        int stringId = ASEngine->GetTypeIdByDecl( "string");
 
         CString* asKey = reinterpret_cast<CString*>( ASEngine->CreateScriptObject( stringType ) );
         CString* asValue = reinterpret_cast<CString*>( ASEngine->CreateScriptObject( stringType ) );
@@ -33,37 +34,39 @@ namespace json
             if( value.is_string() )
             {
                 auto strValue = value.get<std::string>();
-                asValue->assign(strValue.c_str(), strValue.length());
-                ASEXT_CScriptDictionary_Set(dict, asKey, asValue, stringId);
+                asValue->assign( strValue.c_str(), strValue.length() );
+                ASEXT_CScriptDictionary_Set( dict, asKey, asValue, stringId );
             }
-            else if (value.is_number_integer())
+            else if( value.is_number_integer() )
             {
                 int64_t v = value.get<int64_t>();
-                ASEXT_CScriptDictionary_Set(dict, asKey, &v, asTYPEID_INT64);
+                ASEXT_CScriptDictionary_Set( dict, asKey, &v, asTYPEID_INT64 );
             }
-            else if (value.is_number_float())
+            else if( value.is_number_float() )
             {
                 double v = value.get<double>();
-                ASEXT_CScriptDictionary_Set(dict, asKey, &v, asTYPEID_DOUBLE);
+                ASEXT_CScriptDictionary_Set( dict, asKey, &v, asTYPEID_DOUBLE );
             }
-            else if (value.is_boolean())
+            else if( value.is_boolean() )
             {
                 bool v = value.get<bool>();
-                ASEXT_CScriptDictionary_Set(dict, asKey, &v, asTYPEID_BOOL);
+                ASEXT_CScriptDictionary_Set( dict, asKey, &v, asTYPEID_BOOL );
             }
-            else if (value.is_object())
+            else if( value.is_object() )
             {
                 auto asDictionary = reinterpret_cast<CScriptDictionary*>( ASEngine->CreateScriptObject( dictionaryType ) );
-                JsonToDict(value, asDictionary);
+                ToDictionary( value, asDictionary ); // Nested dictionary
                 ASEXT_CScriptDictionary_Set( dict, asKey, &asDictionary, dictionaryId );
                 ASEngine->ReleaseScriptObject( asDictionary, dictionaryType );
+            }
+            else
+            {
+                ALERT( at_console, fmt::format( "JSON Warning ignoring unsuported type implementation \"{}\" for key \"{}\"\n", (int)value.type(), key.c_str() ).c_str() );
             }
         }
 
         ASEngine->ReleaseScriptObject( asKey, stringType );
         ASEngine->ReleaseScriptObject( asValue, stringType );
-
-        return true;
     }
 }
 
@@ -72,7 +75,8 @@ bool SC_SERVER_DECL CASEngineFuncs_JsonDeserialize( void* pthis, SC_SERVER_DUMMY
     try
     {
         auto js = nlohmann::json::parse( (char*)str.c_str() );
-        return json::JsonToDict( js, obj );
+        json::ToDictionary( js, obj );
+        return true;
     }
     catch( nlohmann::json::parse_error& exception )
     {
@@ -82,7 +86,7 @@ bool SC_SERVER_DECL CASEngineFuncs_JsonDeserialize( void* pthis, SC_SERVER_DUMMY
     return true;
 }
 
-bool SC_SERVER_DECL CASEngineFuncs_JsonSerialize(void* pthis, SC_SERVER_DUMMYARG const CScriptDictionary* obj, CString& str, int indents = -1 )
+bool SC_SERVER_DECL CASEngineFuncs_JsonSerialize( void* pthis, SC_SERVER_DUMMYARG const CScriptDictionary* obj, CString& str, int indents = -1 )
 {
     // Iterate over obj which is a dictionary and turn its variables into a json then dump into str
     try
