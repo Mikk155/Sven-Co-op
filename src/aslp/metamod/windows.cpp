d4bbac03e50b259dd76fef1101c23187f4479de0
Mikk155 <martimsanchez155@gmail.com>
2026-03-18 03:05:23 -0300

    fix linux undefined windows API

--- src/aslp/metamod/windows.cpp
+++ src/aslp/metamod/windows.cpp
@@ -1,3 +1,4 @@
+#ifdef _WIN32
 #include <windows.h>
 
 BOOL APIENTRY DllMain( HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved )
@@ -17,3 +18,4 @@ BOOL APIENTRY DllMain( HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReser
 #endif
     return TRUE;
 }
+#endif
