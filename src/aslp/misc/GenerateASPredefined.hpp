24755c984e53ea36cf479244a865da31339daf29
Mikk155 <martimsanchez155@gmail.com>
2026-03-23 13:02:21 -0300

    Maaaaaybe getting started with as.predefined generation

--- src/aslp/misc/GenerateASPredefined.hpp
+++ src/aslp/misc/GenerateASPredefined.hpp
@@ -1,6 +1,3 @@
-// Remove dis shit later since VSC bitches about it. -TODO
-#define AS_GENERATE_DOCUMENTATION 1
-
 // Special thanks to AnggaraNothing for https://github.com/anggaranothing/sc-py-asdocs-vscode
 #if AS_GENERATE_DOCUMENTATION
 #include <string>
@@ -9,26 +6,71 @@
 #include <filesystem>
 #include <ctime>
 #include <chrono>
+#include <atomic>
+#include <thread>
+#include <vector>
+#include <mutex>
 
 #include <fmt/core.h>
 
 #pragma once
 
-#ifdef GENERATE_AS_EXTERNAL_TEST
-#define LOG_ARGS(fmt_str, ...) fmt::print( "[GenerateASPredefined] " fmt_str "\n", __VA_ARGS__ )
-#define LOG(fmt_str) fmt::print( "[GenerateASPredefined] " fmt_str "\n" )
-#else
-#include <extdll.h>
-#include <meta_api.h>
-#include <asext_api.h>
-#define LOG_ARGS(fmt_str, ...) ALERT( at_console, fmt::format( "[GenerateASPredefined] " fmt_str "\n", __VA_ARGS__).c_str() )
-#define LOG(fmt_str, ...) ALERT( at_console, "[GenerateASPredefined] " fmt_str "\n" )
-#endif
+#define LOG_ARGS(fmt_str, ...) { \
+    std::lock_guard<std::mutex> lock(state->bufferMutex); \
+    state->buffer.push_back( fmt::format( "[GenerateASPredefined] " fmt_str "\n", __VA_ARGS__ ) ); }
+#define LOG(fmt_str) { \
+    std::lock_guard<std::mutex> lock(state->bufferMutex); \
+    state->buffer.push_back( "[GenerateASPredefined] " fmt_str "\n" ); }
 
 namespace GenerateASPredefined
 {
 using string = std::string;
 using string_v = std::string_view;
+
+struct ThreadState
+{
+    std::atomic<bool> cancel{false};
+    std::atomic<bool> done{false};
+
+    std::mutex bufferMutex;
+    std::vector<string> buffer;
+};
+
+inline void Generate( ThreadState* state )
+{
+    for( int i = 0; i < 100; ++i )
+    {
+        if( state->cancel.load() )
+            break;
+
+        LOG_ARGS( "Procesing {}", i )
+    }
+
+    state->done = true;
+}
+
+std::thread g_thread;
+ThreadState* g_state = nullptr;
+
+inline void Start()
+{
+    g_state = new ThreadState();
+    g_thread = std::thread( Generate, g_state );
+}
+
+inline void Shutdown()
+{
+    if( g_state != nullptr )
+    {
+        g_state->cancel = true;
+        g_state->buffer.clear();
+        delete g_state;
+        g_state = nullptr;
+    }
+
+    if( g_thread.joinable() )
+        g_thread.join();
+}
 
 // Google AI Slop for now.
 #include <windows.h>
@@ -66,7 +108,6 @@ inline void ClientPutInServer()
 
         if( !pFile )
         {
-            LOG_ARGS( "Couldn't open file \"{}\"\n", path );
             return false;
         }
 
@@ -112,8 +153,6 @@ inline void ClientPutInServer()
     if( !asbaseclasses.empty() )
     {
         convertToUtf8( asbaseclasses );
-
-        LOG_ARGS( "asbaseclasses: {}", asbaseclasses );
     }
 } // End Generate
 } // End GenerateASPredefined
--- test/GenerateASPredefined/main.cpp
+++ test/GenerateASPredefined/main.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <filesystem>
 
 #include "../../src/aslp/misc/GenerateASPredefined.hpp"
@@ -7,6 +8,19 @@ int main()
     // Set new working directory
     std::filesystem::current_path( "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Sven Co-op" );
 
-    GenerateASPredefined::Generate();
+    GenerateASPredefined::Start();
+
+    while( !GenerateASPredefined::g_state->done )
+    {
+
+    }
+
+    for( const std::string& str : GenerateASPredefined::g_state->buffer )
+    {
+        fmt::print( fmt::runtime( str ) );
+    }
+
+    GenerateASPredefined::Shutdown();
+
     return 0;
 }
